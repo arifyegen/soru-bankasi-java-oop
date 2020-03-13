4ba11af51fd35b53390b5340f53592b600a5ea38
omerfruk <omer.fruk3547@gmail.com>
2020-03-13 22:36:50 +0300

    quiz manager eklendi

---
+++ ödev/QuizManager.java
@@ -0,0 +1,35 @@
+public class QuizManager{
+    
+    private String sorular;
+    private String cevaplar;
+
+    public QuizManager(String sorular, String cevaplar){
+        this.cevaplar=cevaplar;
+        this.sorular=sorular;
+    }
+    /**
+     * @return the sorular
+     */
+    public String getSorular() {
+        return sorular;
+    }
+    /**
+     * @param sorular the sorular to set
+     */
+    public void setSorular(String sorular) {
+        this.sorular = sorular;
+    }
+    /**
+     * @return the cevaplar
+     */
+    public String getCevaplar() {
+        return cevaplar;
+    }
+    /**
+     * @param cevaplar the cevaplar to set
+     */
+    public void setCevaplar(String cevaplar) {
+        this.cevaplar = cevaplar;
+    }
+
+}
